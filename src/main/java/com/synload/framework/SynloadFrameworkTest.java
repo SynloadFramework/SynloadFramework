0cc5e1cd98920de53abb645fafb33b45eea0cb32
Nathaniel Davidson <nathaniel.davidson@gmail.com>
2016-02-26 10:46:40 -0800

    JavaDocs

---
+++ src/main/java/com/synload/framework/SynloadFrameworkTest.java
@@ -0,0 +1,14 @@
+package com.synload.framework;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SynloadFrameworkTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
